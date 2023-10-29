32facb3f17cc79d2a36ab977869ea55b5ebbc396
SatchelF <satchcollege@gmail.com>
2023-10-28 20:39:07 -0600

    one more time

--- app/src/main/java/com/usu/preferenceswithmvvm/ui/navigation/Routes.kt
+++
@@ -1,8 +0,0 @@
-package com.usu.preferenceswithmvvm.ui.navigation
-
-data class Screen(val route: String)
-
-object Routes {
-    val home = Screen("home")
-    val preferences = Screen("preferences")
-}
